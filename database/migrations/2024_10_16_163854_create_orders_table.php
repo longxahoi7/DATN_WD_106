51fd1fd3e66346dfa84917c967cadb4d704a7167
Hiuhiu10 <ngoisao00304@gmail.com>
2024-11-02 07:43:06 +0700

    Update 2024_10_16_163854_create_orders_table.php

--- database/migrations/2024_10_16_163854_create_orders_table.php
+++ database/migrations/2024_10_16_163854_create_orders_table.php
@@ -25,7 +25,7 @@ return new class extends Migration
                 'completed',
             ])->default('pending');
             $table->decimal('total', 10, 2);
-            $table->dateTime('invoice_date');
+            $table->timestamp('invoice_date')->nullable();
             $table->enum('payment_status', [
                 'pending',     // Đang chờ xử lý
                 'paid',        // Đã thanh toán
