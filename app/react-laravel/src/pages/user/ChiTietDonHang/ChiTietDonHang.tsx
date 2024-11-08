12e93ec890155b32720f1b83fabffefb557d0dd6
MinhJapan <cnm2432004@gmail.com>
2024-11-09 00:11:45 +0700

    them file chi tiet don hang

---
+++ app/react-laravel/src/pages/user/ChiTietDonHang/ChiTietDonHang.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../../style/ChiTietDonHang.css'
+type Props = {}
+
+const ChiTietDonHang = (props: Props) => {
+  return (
+    <div></div>
+  )
+}
+
+export default ChiTietDonHang
