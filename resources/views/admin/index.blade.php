e12f8dacf86e8b3bea286034d3cdf6cfa66d5904
Hiuhiu10 <ngoisao00304@gmail.com>
2024-12-10 03:16:11 +0700

    Revert "Reapply "Merge branch 'master' into hieu-dang""
    This reverts commit 0187f0ffb4184f6df0279e5700129f4af8b2a6ba.

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,31 +2,30 @@
 <html lang="en">
 
 <head>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
     <meta charset="UTF-8">
-
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Admin')</title>
+    <title>Document</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <!-- Favicon -->
-    <link rel="icon" href="{{ asset('imagePro/image/logo/logoremove-white.png') }}" type="image/png">
-
+    <!-- <link rel="stylesheet" href="{{asset('css/header.css')}}">
+    <link rel="stylesheet" href="{{asset('css/footer.css')}}">
+    <link rel="stylesheet" href="{{asset('css/navbar.css')}}">
+    <link rel="stylesheet" href="{{asset('css/auth.css')}}"> -->
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <!-- <link rel="stylesheet" href="{{asset('css/navbarAmin.css')}}"> -->
+    <link rel="stylesheet" href="{{asset('css/navbarAmin.css')}}">
     @stack('styles')
 </head>
 
 <body>
     <header>
-        @include('admin.layoutAdmin.header-admin')
-        <!-- Include file header -->
+        @include('admin.layoutAdmin.header-admin') <!-- Include file header -->
     </header>
     <section>
-        <!-- @include('admin.layoutAdmin.navbar-admin') -->
-        <!-- Include file header -->
+        @include('admin.layoutAdmin.navbar-admin') <!-- Include file header -->
     </section>
     <main>
         @yield('content')
@@ -35,7 +34,6 @@
 </body>
 
 <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> -->
-
 
 
 </html>
