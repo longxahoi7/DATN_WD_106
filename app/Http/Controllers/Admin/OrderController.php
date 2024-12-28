c04794255ef136f39e79f00218458940eab20077
Hiuhiu10 <ngoisao00304@gmail.com>
2024-12-28 19:49:49 +0700

    update instock admin

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -58,11 +58,24 @@ class OrderController extends Controller
     
         if ($order) {
             $previousStatus = $order->status; // Lưu trạng thái cũ trước khi cập nhật
-    
             // Cập nhật trạng thái đơn hàng
             $order->status = $request->status;
             $order->save();
-    
+            foreach ($order->orderItems as $orderItem) {
+                $product = $orderItem->product;
+            
+                if ($product && $product->attributeProducts) {
+                    $attributeProduct = $product->attributeProducts
+                        ->where('size_id', $orderItem->size_id)
+                        ->first();
+            
+                    if ($attributeProduct) {
+                        // Cộng lại số lượng vào in_stock
+                        $attributeProduct->in_stock += $orderItem->quantity; // Đổi `qty` thành `quantity`
+                        $attributeProduct->save();
+                    }
+                }
+            }
             // Ghi lại lịch sử thay đổi trạng thái
             OrderStatusHistory::create([
                 'order_id' => $order->order_id,
